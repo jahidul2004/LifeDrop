a93265827f5b29ce988920534f759c9c74e1ef81
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-29 14:21:47 +0600

    Navbar completed.

--- src/shared/navbar/NavBar.jsx
+++ src/shared/navbar/NavBar.jsx
@@ -1,8 +1,72 @@
+import { BsDropletFill } from "react-icons/bs";
+import { MdOutlineBloodtype } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    const links = (
+        <>
+            <li>
+                <Link>Home</Link>
+            </li>
+            <li>
+                <Link>About Us</Link>
+            </li>
+            <li>
+                <Link>Find Blood</Link>
+            </li>
+            <li>
+                <Link>Register</Link>
+            </li>
+        </>
+    );
     return (
-        <div>
-            This is Navbar
+        <div className="sticky top-0">
+            <div className="w-[95%] md:container mx-auto navbar">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost lg:hidden"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                {" "}
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />{" "}
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                        >
+                            {links}
+                        </ul>
+                    </div>
+                    <a className="text-2xl font-semibold flex items-center gap-2">
+                        <BsDropletFill size={32} className="text-error" />
+                        Life<span className="text-error">Drop</span>
+                    </a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">{links}</ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn btn-error btn-soft">
+                        <MdOutlineBloodtype />
+                        Join With Us
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
