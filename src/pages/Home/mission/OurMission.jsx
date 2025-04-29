2ffebdcfdf75b1d0b5755cd7591dcc235c2631c7
jahidul2004 <islamjahiduljihad@gmail.com>
2025-04-29 20:28:46 +0600

    Our mission updated.

--- src/pages/Home/mission/OurMission.jsx
+++ src/pages/Home/mission/OurMission.jsx
@@ -1,7 +1,7 @@
 const OurMission = () => {
     return (
-        <div className="bg-[#fff4f1] p-2 md:p-4 lg:p-8 mb-5 rounded">
-            <h1 className="text-3xl font-bold mb-2 text-[#f64245]">
+        <div className="text-center bg-gradient-to-b from-[#fff4f1] p-2 md:p-4 lg:p-8 mb-32 rounded">
+            <h1 className="text-4xl font-bold mb-2 text-[#f64245]">
                 Our Mission
             </h1>
             <p>
